7bcc055c2ebf19d9592a7072f65253a65b9c4c97
Your Name <you@example.com>
2023-10-17 13:27:30 +0200

    clases atomicas ok

--- Alien.cs
+++ Alien.cs
@@ -8,10 +8,22 @@ namespace GalagaConC_
 {
     public class Alien : Ship
     {
+        public int Health { get; set; };
+
         public Alien(int id, object render, object hitbox, int[] position, Projectile projectile) : this(id, render, hitbox, position, projectile, 1)
         { }
-        public Alien(int id, object render, object hitbox, int[] position, Projectile projectile, int health)
-        { }
+        public Alien(int id, object render, object hitbox, int[] position, Projectile projectile, int health = 1)
+        {
+            this.Id = id;
+            this.Render = render;
+            this.Hitbox = hitbox;
+            this.Position = position;
+            this.Projectile = projectile;
+            this.Health= health;
+
+
+
+        }
     }
 
 }
--- PlayerShip.cs
+++ PlayerShip.cs
@@ -8,9 +8,21 @@ namespace GalagaConC_
 {
     public class PlayerShip : Ship
     {
+        bool Shield { get; set; };
+
         public PlayerShip( int id, object render, object hitbox, int[] position, Projectile projectile) : this(id, render, hitbox, position, projectile, true)
         { }
-        public PlayerShip(int id, object render, object hitbox, int[] position, Projectile projectile, bool shield) 
-        {  }
+        public PlayerShip(int id, object render, object hitbox, int[] position, Projectile projectile, bool shield = true) 
+        {
+            this.Id = id;
+            this.Render = render;
+            this.Hitbox = hitbox;
+            this.Position = position;
+            this.Projectile = projectile;
+            this.Shield = shield;
+            
+        
+        
+        }
     }
 }
